9a5b249dd097639e179e5f176ca6f7f58203b9d9
SergioTobon <sestebantmontoya@gmail.com>
2025-09-03 17:35:57 -0500

    se configuro la variable de entorno del registrar con google

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -145,7 +145,7 @@ const Register = () => {
           <div className="google-login-container">
             <p className="or-text">O</p>
             {/* ✅ Añadido target="_blank" aquí */}
-            <a href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google`} className="google-login-btn" target="_blank" rel="noopener noreferrer">
+            <a href={`${process.env.REACT_APP_API_URL}/oauth2/authorization/google`} className="google-login-btn" target="_blank" rel="noopener noreferrer">
               Iniciar sesión con Google
             </a>
           </div>
